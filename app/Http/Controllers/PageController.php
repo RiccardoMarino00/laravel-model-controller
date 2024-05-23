3404461f2a336b59fb4cedb7b09773cb9ff3529c
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-23 15:52:22 +0200

    Added: variable movies in PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use app\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -9,6 +10,8 @@ class PageController extends Controller
     //
     public function index()
     {
-        return view('/');
+        $movies = Movie::all();
+        var_dump($movies);
+        return view('/', compact('movies'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
